de52f00c94bb036116655fea4b57b75272eabee4
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-10-05 13:39:39 +0100

    Remove UnitData.

--- bubble/src/Utils/UnitID.hpp
+++ bubble/src/Utils/UnitID.hpp
@@ -1,33 +1,19 @@
 #ifndef _TKET_UnitID_H_
 #define _TKET_UnitID_H_
 
-#include <memory>
-
 namespace tket {
 
 enum class UnitType { Qubit };
 
 class UnitID {
  public:
-  UnitID() : data_(std::make_shared<UnitData>()) {}
-
- protected:
-  UnitID(UnitType type) : data_(std::make_shared<UnitData>(type)) {}
-
- private:
-  struct UnitData {
-    UnitType type_;
-
-    UnitData() : type_(UnitType::Qubit) {}
-    UnitData(UnitType type) : type_(type) {}
-  };
-  std::shared_ptr<UnitData> data_;
+  UnitID() {}
 };
 
 /** Location holding a qubit */
 class Qubit : public UnitID {
  public:
-  Qubit() : UnitID(UnitType::Qubit) {}
+  Qubit() : UnitID() {}
 };
 
 }  // namespace tket
