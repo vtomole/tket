eb519fb94dc448a2262798bf4a416d02061f776a
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-10-05 13:33:51 +0100

    Remove name_.

--- bubble/src/Utils/UnitID.hpp
+++ bubble/src/Utils/UnitID.hpp
@@ -1,64 +1,30 @@
-// Copyright 2019-2021 Cambridge Quantum Computing
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef _TKET_UnitID_H_
 #define _TKET_UnitID_H_
 
-/**
- * @file
- * @brief Named registers of arrays of (quantum or classical) nodes
- */
-
-#include <map>
-#include <optional>
-#include <regex>
-#include <set>
-#include <sstream>
-#include <string>
+#include <memory>
+#include <vector>
 
 namespace tket {
 
-/** Type of information held */
 enum class UnitType { Qubit };
 
-/**
- * Location holding a bit or qubit of information
- *
- * Each location has a name (signifying the 'register' to which it belongs) and
- * an index within that register (which may be multi-dimensional).
- */
 class UnitID {
  public:
   UnitID() : data_(std::make_shared<UnitData>()) {}
 
  protected:
-  UnitID(
-      const std::string &name, const std::vector<unsigned> &index,
-      UnitType type)
-      : data_(std::make_shared<UnitData>(name, index, type)) {}
+  UnitID(const std::vector<unsigned> &index, UnitType type)
+      : data_(std::make_shared<UnitData>(index, type)) {}
 
  private:
   struct UnitData {
-    std::string name_;
     std::vector<unsigned> index_;
     UnitType type_;
 
-    UnitData() : name_(), index_(), type_(UnitType::Qubit) {}
-    UnitData(
-        const std::string &name, const std::vector<unsigned> &index,
+    UnitData() : index_(), type_(UnitType::Qubit) {}
+    UnitData(const std::vector<unsigned> &index,
         UnitType type)
-        : name_(name), index_(index), type_(type) {
+        : index_(index), type_(type) {
     }
   };
   std::shared_ptr<UnitData> data_;
@@ -67,7 +33,7 @@ class UnitID {
 /** Location holding a qubit */
 class Qubit : public UnitID {
  public:
-  Qubit() : UnitID("", {}, UnitType::Qubit) {}
+  Qubit() : UnitID({}, UnitType::Qubit) {}
 };
 
 }  // namespace tket
