554a03f9c2c5bb8acde3ebdade7dd3f074e36521
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-10-05 13:27:35 +0100

    Remove includes.

--- pytket/binders/circuit/main.cpp
+++ pytket/binders/circuit/main.cpp
@@ -13,9 +13,6 @@
 // limitations under the License.
 
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/operators.h>
-#include <sstream>
 #include "Utils/UnitID.hpp"
 
 namespace py = pybind11;
@@ -23,9 +20,7 @@ namespace py = pybind11;
 namespace tket {
 
 PYBIND11_MODULE(circuit, m) {
-  py::enum_<UnitType>(
-      m, "UnitType",
-      "Enum for data types of units in circuits (e.g. Qubits vs Bits).")
+  py::enum_<UnitType>(m, "UnitType", "Enum for data types of units in circuits (e.g. Qubits vs Bits).")
       .value("qubit", UnitType::Qubit, "A single Qubit");
   py::class_<UnitID>(m, "UnitID", "A handle to a computational unit (e.g. qubit, bit)");
   py::class_<Qubit, UnitID>(m, "Qubit", "A handle to a qubit");
