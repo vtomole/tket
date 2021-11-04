f4cb5fa8b32c1eb9f7a72670ef3347fb15e29956
«Deploy tket C++ documentation» github action <tket-bot@cambridgequantum.com>
2021-11-04 10:46:10 +0000

    Add generated tket C++ documentation.

--- docs/tket/api/search/files_0.js
+++ docs/tket/api/search/files_0.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['adjacencydata_2ecpp_2239',['AdjacencyData.cpp',['../AdjacencyData_8cpp.html',1,'']]],
   ['adjacencydata_2ehpp_2240',['AdjacencyData.hpp',['../AdjacencyData_8hpp.html',1,'']]],
-  ['architectures_2ecpp_2241',['Architectures.cpp',['../Architectures_8cpp.html',1,'']]],
-  ['architectures_2ehpp_2242',['Architectures.hpp',['../Architectures_8hpp.html',1,'']]],
+  ['architecture_2ecpp_2241',['Architecture.cpp',['../Architecture_8cpp.html',1,'']]],
+  ['architecture_2ehpp_2242',['Architecture.hpp',['../Architecture_8hpp.html',1,'']]],
   ['articulationpoints_2ecpp_2243',['ArticulationPoints.cpp',['../ArticulationPoints_8cpp.html',1,'']]],
   ['articulationpoints_2ehpp_2244',['ArticulationPoints.hpp',['../ArticulationPoints_8hpp.html',1,'']]],
   ['articulationpoints_5fimpl_2ehpp_2245',['ArticulationPoints_impl.hpp',['../ArticulationPoints__impl_8hpp.html',1,'']]],
