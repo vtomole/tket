5ceaae6022571715f25c2bc6c533a4e1ce4d1aca
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-11-24 21:31:41 +0000

    Replace conanfile for symengine with former design.

--- recipes/symengine/conanfile.py
+++ recipes/symengine/conanfile.py
@@ -15,46 +15,15 @@
 from conans import ConanFile, CMake, tools
 import os
 
-required_conan_version = ">=1.33.0"
-
 
 class SymengineConan(ConanFile):
     name = "symengine"
     version = "0.8.1.1"
-    description = "A fast symbolic manipulation library, written in C++"
-    license = "MIT"
-    topics = ("symbolic", "algebra")
-    homepage = "https://symengine.org/"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "integer_class": ["boostmp", "gmp"],
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-        "integer_class": "boostmp",
-    }
-    short_paths = True
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def requirements(self):
-        if self.options.integer_class == "boostmp":
-            self.requires("boost/1.77.0")
-        else:
-            self.requires("gmp/6.2.1")
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    requires = "boost/1.77.0"
 
     def source(self):
         git = tools.Git()
@@ -64,41 +33,38 @@ class SymengineConan(ConanFile):
             shallow=True,
         )
 
-    def _configure_cmake(self):
-        if self._cmake is None:
-            self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_TESTS"] = False
-            self._cmake.definitions["BUILD_BENCHMARKS"] = False
-            self._cmake.definitions["INTEGER_CLASS"] = self.options.integer_class
-            self._cmake.definitions["MSVC_USE_MT"] = False
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
-
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        # source_folder = os.path.join(self.source_folder, f"symengine-{self.version}")
+        tools.replace_in_file(
+            os.path.join(self.source_folder, "CMakeLists.txt"),
+            "project(symengine)",
+            """project(symengine)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()""",
+        )
+        cmake.configure(
+            source_folder=self.source_folder,
+            defs={
+                "BUILD_TESTS": "OFF",
+                "BUILD_BENCHMARKS": "OFF",
+                "INTEGER_CLASS": "boostmp",
+                "MSVC_USE_MT": "OFF",
+                "WITH_SYMENGINE_TEUCHOS": "yes",
+            },
+        )
+
         cmake.build()
+        cmake.install()
+        cmake.patch_config_paths()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # [CMAKE-MODULES-CONFIG-FILES (KB-H016)]
-        tools.remove_files_by_mask(self.package_folder, "*.cmake")
-        # [DEFAULT PACKAGE LAYOUT (KB-H013)]
-        tools.rmdir(os.path.join(self.package_folder, "CMake"))
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*symengine.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["symengine"]
-        if any("teuchos" in v for v in tools.collect_libs(self)):
-            self.cpp_info.libs.append("teuchos")
-        self.cpp_info.names["cmake_find_package"] = "symengine"
-        # FIXME: symengine exports a non-namespaced `symengine` target.
-        self.cpp_info.names["cmake_find_package_multi"] = "symengine"
+        self.cpp_info.libs = ["symengine", "teuchos"]
